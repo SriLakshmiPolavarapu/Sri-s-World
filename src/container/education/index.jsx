34206162f81d3945f61cfc6068a8c981b4933f57
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2024-07-18 00:43:23 -0700

    started with education section

--- src/container/education/index.jsx
+++ src/container/education/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageHeaderContent from '../../components/pageHeaderContent';
+import './styles.scss';
 
 const Education = () =>
 {
@@ -8,6 +9,13 @@ const Education = () =>
         <PageHeaderContent
         headerText = "Education"
         />
+
+        <div className='education-info'>
+            <h2 className='education-field'>Master's in Computer Science</h2>
+            <h2 className='education-college'>Portland State University</h2>
+            <i className='education-date'>September 2023 - June 2025</i>
+            <h3 className='education-percentage'>GPA: 3.7 / 4</h3>
+        </div>
         </section>
     )
 }
