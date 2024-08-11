daf50a11ff934c839b9c79af7024006dbec626e0
Sri Lakshmi Polavarapu <srilakshmi.polavarapu01@gmail.com>
2024-08-10 17:04:35 -0700

    projects added

--- src/container/projects/index.jsx
+++ src/container/projects/index.jsx
@@ -9,11 +9,23 @@ const projects = [
     link: 'https://github.com/SriLakshmiPolavarapu/generate-recipe',
     skills: ['Python (Flask)','Natural Language Processing','Large Language Model','Frontend Web Development','Reactjs', 'HTML', 'CSS' , 'Javascript','API']
   },
+  {
+    title: 'Dictionary Application',
+    description: 'Formulated a personalized dictionary utilizing  Trie data structure, \nincorporating functions for inserting words, retrieving meanings of those words, deleting, enhancing functionality with capabilities for efficiently adding over 10,000 words.    ',
+    link: 'https://github.com/SriLakshmiPolavarapu/Trie-Dictionary-App',
+    skills: ['Java', 'OOPS', 'Data Structures and Algorithms']
+  },
   { 
     title: 'Goal Getter Dashboard', 
     description: 'A Frontend Web Development project using Reactjs with Vite as development environment. Utilized Axios for API interactions of weather, location and quotes. \nThis dashboard generates random quotes to inspire users and provides a 7-day weather forecast. Displays air quality information and UV Index. \nFeatures a map to calculate distance and travel time between locations. \nIt also has a feature to select the exercise mode (walk/run/jog/cycle).',
     link: 'https://github.com/SriLakshmiPolavarapu/GoalGetter_Dashboard',
     skills: ['Frontend Web Development','Reactjs', 'HTML', 'CSS' , 'Javascript', 'API']
+  },
+  {
+    title: 'Snakes and Ladder Game',
+    description: 'A Snake and Ladder game implemented using principles of Object-Oriented Programming and Data Structures in Java. \n Utilized Java Swing and Java applets for GUI elements. \nCreated backend data for users to play the game using the command line. Employed OOP concepts to organize code and enhance maintainability of code.',
+    link: 'https://github.com/SriLakshmiPolavarapu/Snake-and-Ladder',
+    skills: ['Java', 'OOPS', 'Java Applets', 'Data Structures and Algorithms']
   },
   { 
     title: 'Color Transfer Between Images', 
@@ -38,12 +50,6 @@ const projects = [
     description: 'A Machine Learning project built using various Machine Learning algorithms (K-means, Random Forest, Logistic Regression, Naive Bayes and Decision Tree Classifier). \nUtilized Rpubs and Tableau tool for visualizing the patterns. \nGained an accuracy of 95% for Random Forest, 92% for K-means, 92.05% for Logistic Regression, 90% for Decision Tree, 87% for Naive Bayes Classifier.',
     link: 'https://github.com/SriLakshmiPolavarapu/Analysis-of-Indian-Premier-League-Data',
     skills: ['Machine Learning', 'Data Analytics', 'Data Mining', 'Python', 'Colab', 'Rstudio']
-  },
-  {
-    title: 'Snakes and Ladder Game',
-    description: 'A Snake and Ladder game implemented using principles of Object-Oriented Programming and Data Structures in Java. \n Utilized Java Swing and Java applets for GUI elements. \nCreated backend data for users to play the game using the command line. Employed OOP concepts to organize code and enhance maintainability of code.',
-    link: 'https://github.com/SriLakshmiPolavarapu/Snake-and-Ladder',
-    skills: ['Java', 'OOPS', 'Java Applets', 'Data Structures and Algorithm']
   },
   {
     title: 'Hotel Management System',
